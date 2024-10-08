daacbbdfa0f966a21f4f87894cd62004bcc1295c
Nischal Silwal <nischalsilwalhtd@gmail.com>
2024-10-08 13:59:06 +0545

    Add project files.

---
+++ Product_CURD_API/Controllers/ProductController.cs
@@ -0,0 +1,65 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Http.HttpResults;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Product_CURD_API.Data;
+using Product_CURD_API.Model;
+using static Microsoft.EntityFrameworkCore.DbLoggerCategory;
+
+namespace Product_CURD_API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ProductController : ControllerBase
+    {
+        private readonly DemoDbContext _DemoDbContext;
+        public ProductController(DemoDbContext demoDbContext)
+        {
+            _DemoDbContext = demoDbContext;
+        }
+        [HttpGet]
+        //Get All Products
+        public ActionResult<IEnumerable<Product>> Get()
+        {
+            return _DemoDbContext.Products;
+        }
+        [HttpGet("{id}")]
+        //Get Product by ID
+        public async Task<ActionResult<Product>> GetById(int id)
+        {
+            return await _DemoDbContext.Products.Where(x=> x.Id == id).SingleOrDefaultAsync();
+        }
+
+        [HttpPost]
+        //Create a Product
+        public async Task<ActionResult> Create(Product product)
+        {
+            await _DemoDbContext.Products.AddAsync(product);
+            await _DemoDbContext.SaveChangesAsync();
+
+            return CreatedAtAction(nameof(GetById), new { id = product.Id }, product);
+        }
+
+        [HttpPut]
+       // Update a Product
+        public async Task<ActionResult> Update(Product product)
+        {
+            _DemoDbContext.Products.Update(product);
+            await _DemoDbContext.SaveChangesAsync();
+            return Ok();
+        }
+        [HttpDelete]
+        //Delete a Product
+        public async Task<ActionResult> Delete(int id)
+        {
+            var ProductGetById = await GetById(id);
+            if (ProductGetById == null)
+            {
+                return NotFound();
+            }
+            _DemoDbContext.Remove(ProductGetById.Value);
+            await _DemoDbContext.SaveChangesAsync();
+            return Ok();
+        }
+    }
+}
---
+++ Product_CURD_API/Data/DemoDbContext.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.Identity.Client;
+using Product_CURD_API.Model;
+
+namespace Product_CURD_API.Data
+{
+    public class DemoDbContext : DbContext
+    {
+        public DemoDbContext(DbContextOptions dbContextOptions) : base (dbContextOptions)
+        {
+            
+        }
+        public DbSet<Product> Products { get; set; }
+
+    }
+    
+}
